15286e7ae5ce546601eb72e8786ab302fc83fa59
Sebastian Delmont <sd@notso.net>
2023-03-03 11:33:11 -0500

    Updated to latest Country File

--- src/lib/analyzeFromCountryFile.spec.js
+++ src/lib/analyzeFromCountryFile.spec.js
@@ -13,7 +13,7 @@ describe("Country File analyzis and annotation", () => {
 
     it("should find the Country File version", () => {
       const info = analyzeFromCountryFile({ call: "VERSION" })
-      expect(info.entityName).toEqual("Marshall Islands")
+      expect(info.entityName).toEqual("Eritrea")
     })
 
     it("should annotate from a DXCC code", () => {
