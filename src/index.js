dcd72151db14cd60c8d329f88911822d225f24d0
Sebastian Delmont <sd@notso.net>
2023-10-09 09:33:22 -0400

    Moved builindata exports to the main index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const { parseCountryFile } = require('./lib/parseCountryFile')
 const { setCountryFileData, annotateFromCountryFile, fillDXCCFromCountryFile } = require('./lib/analyzeFromCountryFile')
+const { setCountryFileData } = require('./lib/analyzeFromCountryFile')
+const CTYData = require('./data/bigcty.json')
+
+function useBuiltinCountryFile () {
+  setCountryFileData(CTYData)
+}
 
 module.exports = {
   annotateFromCountryFile,
   parseCountryFile,
   fillDXCCFromCountryFile,
   setCountryFileData
+  CTYData,
+  useBuiltinCountryFile
 }
