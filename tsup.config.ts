99e7392fbe1b4feea6dcb0fa8e1fdadd45d8939c
Sebastian Delmont <sd@notso.net>
2026-01-07 07:41:13 -0500

    Support CJS

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist',
+  splitting: false,
+  treeshake: true,
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.js',
+    }
+  },
+})
+
